b911897f0201ab566e7abf018636ea1a8e802d09
Princi02 <priyanshimangal891@gmail.com>
2025-03-01 22:13:25 +0530

    hello with params

--- src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.example.hellomessagingapp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,5 +9,10 @@ public class HelloRestController {
     @RequestMapping("/")
     public String sayhello(){
         return "Hello From BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
