6b8dd974578ce6ad37a3a66fa59527d94f4091de
Princi02 <priyanshimangal891@gmail.com>
2025-03-01 21:59:59 +0530

    Hello message

---
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -0,0 +1,11 @@
+package com.example.hellomessagingapp.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String sayhello(){
+        return "Hello From BridgeLabz";
+    }
+}
